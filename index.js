d8b59d6a1d82b24915b46ce10570ad840d0d81ce
BenjieEdroso <ben09099@gmail.com>
2022-06-19 04:34:53 +0800

    added next and prev btn

---
+++ index.js
@@ -0,0 +1,9 @@
+const rightBtn = document.querySelector(".right-btn");
+const leftBtn = document.querySelector(".left-btn");
+
+rightBtn.addEventListener("click", () => {
+  console.log("next");
+});
+leftBtn.addEventListener("click", () => {
+  console.log("prev");
+});
