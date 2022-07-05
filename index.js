bdb4e0ace5c567b6e4592671586bd0a08e3ce3a2
BenjieEdroso <ben09099@gmail.com>
2022-07-06 01:02:46 +0800

    added new functions

--- index.js
+++ index.js
@@ -1,79 +1,21 @@
-const rightBtn = document.querySelector(".right-btn");
-const leftBtn = document.querySelector(".left-btn");
-let maxOffset = 18;
-let calcOffset = null;
-let diffOffset = 10;
-let categoriesCont = document.querySelector(".categories");
-let latestCards = document.querySelector(".latest-slider");
-//input - btn click state, 18% translate, percent left, and percent decreased
+let latestSlider = document.querySelector(".latest");
+let latestRightBtn = document.querySelector(".latest-right-btn");
+let latestLeftBtn = document.querySelector(".latest-left-btn");
+let categorySlider = document.querySelector(".slider");
+let categoryLeftBtn = document.querySelector(".category-left-btn");
+let categoryRightBtn = document.querySelector(".category-right-btn");
 
-function rightChevronTransition() {
-  if (offSet < 20) {
-    categoriesCont.style = `transform: translateX(-${(offSet = offSet + 10)}%)`;
-  }
-  if (offSet === 20) {
-    rightBtn.classList.add("hide-btns");
-    leftBtn.classList.remove("hide-btns");
-  } else if (offSet <= 20) {
-    rightBtn.classList.remove("hide-btns");
-    leftBtn.classList.remove("hide-btns");
-  }
-}
-
-function leftChevronTransition() {
-  if (offSet > 0) {
-    categoriesCont.style = `transform: translateX(-${(offSet = offSet - 10)}%)`;
-  }
-  if (offSet === 0) {
-    leftBtn.classList.add("hide-btns");
-    rightBtn.classList.remove("hide-btns");
-  } else if (offSet >= 0) {
-    leftBtn.classList.remove("hide-btns");
-    rightBtn.classList.remove("hide-btns");
-  }
-}
-
-let offSet = 0;
-rightBtn.addEventListener("click", (e) => {
-  rightChevronTransition();
+categorySlider.addEventListener("mouseover", () => {
+  categoryRightBtn.classList.add("show-btn");
 });
-leftBtn.addEventListener("click", () => {
-  leftChevronTransition();
+categorySlider.addEventListener("mouseleave", () => {
+  categoryRightBtn.classList.remove("show-btn");
 });
 
-let rightBtns = document.querySelectorAll(".right-btn");
-let leftBtns = document.querySelectorAll(".left-btn");
-
-let offSet2 = 0;
-let counter = 0;
-rightBtns.forEach((buttons) => {
-  buttons.addEventListener("click", (e) => {
-    if (e.target.classList.contains("latest-btn")) {
-      counter++;
-      if (counter <= 7) {
-        if (counter < 2) {
-          latestCards.style = `transform: translateX(-${(offSet2 =
-            offSet2 + 15)}%)`;
-        } else if (counter === 7) {
-          latestCards.style = `transform: translateX(-${(offSet2 =
-            offSet2 + 2)}%)`;
-        } else {
-          latestCards.style = `transform: translateX(-${(offSet2 =
-            offSet2 + 30)}%)`;
-        }
-      }
-    }
-  });
+latestSlider.addEventListener("mouseover", () => {
+  latestRightBtn.classList.add("show-btn");
 });
 
-leftBtns.forEach((buttons) => {
-  buttons.addEventListener("click", () => {});
+latestSlider.addEventListener("mouseleave", () => {
+  latestRightBtn.classList.remove("show-btn");
 });
-
-let latestSection = document.querySelector(".latest");
-latestSection.addEventListener("mouseover", () => {
-  rightBtn.style = "opacity: 1";
-});
-
-let latestSlider = document.querySelector(".latest-slider");
-latestSlider.addEventListener("mouseover", () => {});
